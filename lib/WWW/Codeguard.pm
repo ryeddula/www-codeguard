7040f02f1630b24cb68558330b4e637a2730f2e2
David Oswald <david.oswald@endurance.com>
2015-07-31 17:11:27 -0600

    Bump version to 0.03.

--- lib/WWW/Codeguard.pm
+++ lib/WWW/Codeguard.pm
@@ -13,11 +13,11 @@ WWW::Codeguard - Perl interface to interact with the Codeguard API
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
